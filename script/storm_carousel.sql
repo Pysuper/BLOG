de3c8580cc7bd6abde0600c4e5b8acd61e77b61a
Pysuper <1821764535@qq.com>
2019-10-08 16:38:46 +0800

    修改为静态服务器

--- script/storm_carousel.sql
+++ script/storm_carousel.sql
@@ -1,7 +1,7 @@
-insert into storm_carousel values (1, 1, '远山', '暮色山水', '/static/images/index1.jpg', '#');
-insert into storm_carousel values (2, 2, '山巅', '登山', '/static/images/index2.jpg', '#');
-insert into storm_carousel values (3, 3, '小僵尸跳', '小僵尸', '/static/images/index3.jpg', '#');
-insert into storm_carousel values (4, 4, 'Windows', 'Windows10', '/static/images/index4.jpg', '#');
-insert into storm_carousel values (5, 5, 'MACOS', 'mac', '/static/images/index5.jpg', '#');
-insert into storm_carousel values (6, 6, 'Gan', 'Gan', '/static/images/index6.jpg', '#');
-insert into storm_carousel values (7, 7, 'moonlight', 'moonlight', '/static/images/index7.jpg', '#');
+insert into storm_carousel values (1, 1, '远山', '暮色山水', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index1.jpg', '#');
+insert into storm_carousel values (2, 2, '山巅', '登山', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index2.jpg', '#');
+insert into storm_carousel values (3, 3, '小僵尸跳', '小僵尸', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index3.jpg', '#');
+insert into storm_carousel values (4, 4, 'Windows', 'Windows10', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index4.jpg', '#');
+insert into storm_carousel values (5, 5, 'MACOS', 'mac', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index5.jpg', '#');
+insert into storm_carousel values (6, 6, 'Gan', 'Gan', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index6.jpg', '#');
+insert into storm_carousel values (7, 7, 'moonlight', 'moonlight', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index7.jpg', '#');
