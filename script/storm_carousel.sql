dafc10373450f9a32dc41cd4358d21c6f7cf5193
PySuper <1821764535@qq.com>
2019-10-22 13:28:19 +0800

    同步线上代码

--- script/storm_carousel.sql
+++ script/storm_carousel.sql
@@ -1,7 +1,7 @@
-insert into storm_carousel values (1, 1, '远山', '暮色山水', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index1.jpg', '#');
-insert into storm_carousel values (2, 2, '山巅', '登山', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index2.jpg', '#');
-insert into storm_carousel values (3, 3, '小僵尸跳', '小僵尸', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index3.jpg', '#');
-insert into storm_carousel values (4, 4, 'Windows', 'Windows10', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index4.jpg', '#');
-insert into storm_carousel values (5, 5, 'MACOS', 'mac', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index5.jpg', '#');
-insert into storm_carousel values (6, 6, 'Gan', 'Gan', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index6.jpg', '#');
-insert into storm_carousel values (7, 7, 'moonlight', 'moonlight', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index7.jpg', '#');
+insert into storm_carousel values (1, 1, 'BLOG', '暮色山水', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index1.jpg', 'http://47.103.202.63/article/1/');
+insert into storm_carousel values (2, 2, 'Scrapy 基础', '登山', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index2.jpg', 'http://47.103.202.63/article/4/');
+insert into storm_carousel values (3, 3, 'Django 站点管理', '小僵尸', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index3.jpg', 'http://47.103.202.63/article/14/');
+insert into storm_carousel values (4, 4, 'HTML 基础', 'Windows10', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index4.jpg', 'http://47.103.202.63/article/8/');
+insert into storm_carousel values (5, 5, '我们啊', 'mac', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index5.jpg', '#');
+insert into storm_carousel values (6, 6, 'Python 基础', 'Python 基础', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index6.jpg', 'http://47.103.202.63/article/2/');
+insert into storm_carousel values (7, 7, 'MySQL 配置', 'MySQL 配置', 'http://pyh05v5q8.bkt.clouddn.com/static/images/index7.jpg', 'http://47.103.202.63/article/6');
